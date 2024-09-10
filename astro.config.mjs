9452490061ee08f319ca3acb906e1448ff067445
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-09-10 19:37:50 -0400

    3 writeups + site changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,54 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+// import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  integrations: [
+    starlight({
+      title: "BCSC",
+      customCss: ["./src/styles/custom.css", "./src/styles/katex.min.css"],
+      components: {
+        Head: "./src/components/Head.astro",
+      },
+      social: {
+        github: "https://github.com/0xdeis",
+        discord: "https://discord.gg/sT7J97rDQ4",
+      },
+      sidebar: [
+        {
+          label: "Fundamentals",
+          autogenerate: { directory: "fundamentals" },
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "writeups",
+          autogenerate: { directory: "write-ups", collapsed: true },
+          badge: { text: "New", variant: "tip" },
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/0xdeis/0xdeis.com/edit/main/",
+      },
+      expressiveCode: {
+        // plugins: [pluginShiki()],
+      },
+    }),
+  ],
 });
